b380a3966a117d43995727688b143810da77b715
Nhat Nguyen Van (SM) <nvnhat.17ck1@gmail.com>
2025-04-02 13:40:55 +0700

    fix: header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,9 +4,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <div>
       <MainLayoutHeader />
-      <div className="container">
-        <main>{children}</main>
-      </div>
+      <main>{children}</main>
     </div>
   );
 }
--- src/components/layouts/MainLayoutHeader/MainLayoutHeader.tsx
+++ src/components/layouts/MainLayoutHeader/MainLayoutHeader.tsx
@@ -36,22 +36,24 @@ export default function MainLayoutHeader() {
   return (
     <div className="sticky left-0 right-0 top-0 z-50 md:py-4">
       <div className="container mx-auto">
-        <div
-          className={cn(
-            'flex h-full items-center justify-between transition-all duration-300 py-4 md:py-2 rounded-lg md:-mx-4 md:px-4',
-            isScrolled && 'md:bg-white md:dark:bg-accent',
-          )}
-        >
-          <Link href="/" className="dark:invert">
-            <Image src={Logo} alt="logo" width={100} height={100} />
-          </Link>
-          <DesktopMenu />
-          <div className="flex items-center gap-4">
-            <ModeToggle />
-            <MobileMenu />
-            <Link href={ROUTES.SIGN_IN} className="max-md:hidden">
-              <Button className="uppercase">LOGIN</Button>
+        <div className="-mx-4 px-4">
+          <div
+            className={cn(
+              'flex h-full items-center justify-between transition-all duration-500 py-4 md:py-2 rounded-lg w-full !mx-auto',
+              isScrolled && 'md:bg-white md:dark:bg-accent md:w-[600px] md:shadow-sm md:px-4',
+            )}
+          >
+            <Link href="/" className="dark:invert">
+              <Image src={Logo} alt="logo" width={100} height={100} />
             </Link>
+            <DesktopMenu />
+            <div className="flex items-center gap-4">
+              <ModeToggle />
+              <MobileMenu />
+              <Link href={ROUTES.SIGN_IN} className="max-md:hidden">
+                <Button className="uppercase">LOGIN</Button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
